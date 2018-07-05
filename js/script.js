97201e249b606a39f6a41165556308cdcfb5702b
wangmeng <1134254837@qq.com>
2018-07-05 16:55:55 +0800

    准备上交

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 * @Author: Marte
 * @Date:   2018-07-05 15:42:36
 * @Last Modified by:   Marte
-* @Last Modified time: 2018-07-05 15:47:02
+* @Last Modified time: 2018-07-05 16:55:22
 */
 
-console.log("我是垃圾")
+console.log("完成");
